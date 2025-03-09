05951d829e30291999ed132665eaf648f17d2dd0
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-09 13:06:54 +0530

    Add files via upload

---
+++ Main3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float p,r,t,s;
+    system("CLS");
+    cout<<"Enter the Principal Amount ";
+    cin>>p;
+    cout<<"Enter the Rate of Interest ";
+    cin>>r;
+    cout<<"Enter the Time Period ";
+    cin>>t;
+    s=(p*r*t)/100;
+    cout<<"Simple Interest is "<<s;
+    return 0;
+}
