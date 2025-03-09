3cad25cb94694b00c6150d0c9da757b5edd78f10
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-09 12:20:50 +0530

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    system("CLS");
+    cout<<"Enter first number ";
+    cin>>i;
+    cout<<"Enter second number ";
+    cin>>j;
+    k=i+j;
+    cout<<"Result is "<<k;
+    getch();
+    return 0;
+}
