134248827e1a7249cbbf35046c7c1652ccbbc7c9
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-09 12:30:59 +0530

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int main()
 {
-    int i,j,k;
+    int i,j,k,l,m,n;
     system("CLS");
     cout<<"Enter first number ";
     cin>>i;
     cout<<"Enter second number ";
     cin>>j;
     k=i+j;
-    cout<<"Result is "<<k;
+    l=i-j;
+    m=i*j;
+    n=i/j;
+    cout<<"Addition is "<<k<<endl;
+    cout<<"Substraction is "<<l<<endl;
+    cout<<"Multiplication is "<<m<<endl;
+    cout<<"Division is "<<n<<endl;
     getch();
     return 0;
-}
+} 
