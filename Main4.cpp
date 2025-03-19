961194b27b3941d9ea4fa62faa58ac0964944c74
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-19 16:30:41 +0530

    Update Main4.cpp

--- Main4.cpp
+++ Main4.cpp
@@ -1,16 +1,17 @@
 // C++ Program to scan and print values using array.
 #include <iostream>
 #include <conio.h>
+using namespace std;
 int main ()
 {
     int arr[5],i;
     system("cls");
-    std::cout<<"Enter 5 numbers: \n";
+    cout<<"Enter 5 numbers: \n";
     for(i=0;i<5;i++)
-    std::cin >> arr[i];
-    std::cout<<"\n Array values are "<<std::endl;
+    cin >> arr[i];
+    cout<<"\n Array values are "<<endl;
     for(i=0;i<5;i++)
-    std::cout<<arr[i]<<std::endl;
-    std::cin.get();
+    cout<<arr[i]<<std::endl;
+    cin.get();
     return 0;
 }
