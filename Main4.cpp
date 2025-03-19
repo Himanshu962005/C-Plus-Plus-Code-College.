bca86fb17ca4e8aa48a2f550a1ac3057ff8c4528
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-19 16:41:48 +0530

    Update Main4.cpp

--- Main4.cpp
+++ Main4.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-int main ()
+int main()
 {
-    int arr[5],i;
+    int arr[5], i;
     system("cls");
-    cout<<"Enter 5 numbers: \n";
-    for(i=0;i<5;i++)
-    cin >> arr[i];
-    cout<<"\n Array values are "<<endl;
-    for(i=0;i<5;i++)
-    cout<<arr[i]<<std::endl;
+    cout << "Enter 5 numbers: \n";
+    for (i = 0; i < 5; i++)
+        cin >> arr[i];
+    cout << "\n Array values are " << endl;
+    for (i = 0; i < 5; i++)
+        cout << arr[i] << std::endl;
     cin.get();
     return 0;
 }
