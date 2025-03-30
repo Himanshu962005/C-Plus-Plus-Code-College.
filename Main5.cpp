8c357432459120305265709b2cc6d69eed5adb33
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-30 16:35:55 +0530

    Update Main5.cpp

--- Main5.cpp
+++ Main5.cpp
@@ -4,18 +4,22 @@
 using namespace std;
 int main()
 {
-    int arr[5], i, max;
+    int arr[5], i, max, min;
     system("cls");
     cout << "Enter 5 numbers: \n";
     for (i = 0; i < 5; i++)
         cin >> arr[i];
     max = arr[0];
+    min = arr[0];
     for (i = 1; i < 5; i++)
     {
         if (max < arr[i])
             max = arr[i];
+        if (min > arr[i])
+            min = arr[i];
     }
     cout << "The largest element = " << max;
+    cout << "\nThe Smallest element = " << min;
     getch();
     return 0;
 }
