150362fe0f7857436dbab772912e75095fc96910
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-05 11:31:14 +0530

    Update Main6.cpp

--- Main6.cpp
+++ Main6.cpp
@@ -1,10 +1,10 @@
-// C++ Program sum of two matrices.
+// C++ Program Addition, Substraction and Multiplication of two matrices.
 #include <iostream>
 #include <conio.h>
 using namespace std;
 int main()
 {
-    int i, j, m1[3][3], m2[3][3], sum[3][3], sub[3][3];
+    int i, j, m1[3][3], m2[3][3], sum[3][3], sub[3][3], mul[3][3];
     system("cls");
     cout << "Enter the elements of First matrices\n";
     for (i = 0; i < 3; i++)
@@ -57,6 +57,16 @@ int main()
         {
             sub[i][j] = m1[i][j] - m2[i][j];
             cout << sub[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "Mul of two matrices\n";
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            mul[i][j] = m1[i][j] * m2[i][j];
+            cout << mul[i][j] << "\t";
         }
         cout << endl;
     }
