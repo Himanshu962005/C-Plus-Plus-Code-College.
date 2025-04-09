bddf17d5d83a3cbbbe5e763ca1aaa84858b56cf2
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-09 22:45:09 +0530

    Update Main10.cpp

--- Main10.cpp
+++ Main10.cpp
@@ -1,10 +1,8 @@
-// //C++ program to insert node at the beginning of double linked list.
+// C++ program to Insert Node at the end of single linked list.
 #include <iostream>
 using namespace std;
-// structure for NODE
 struct NODE
 {
-    NODE *prev;
     int data;
     NODE *next;
 };
@@ -18,8 +16,8 @@ public:
     {
         start = end = NULL;
     }
-    // Function to insert at the beginning in double linked list
-    void insert_beginning(int d)
+    // Function to insert at the end
+    void insert_end(int d)
     {
         temp = new NODE; // allocate memory to new node
         temp->data = d;
@@ -28,18 +26,15 @@ public:
         {
             start = end = temp;
             end->next = NULL;
-            end->prev = NULL;
         }
         else
         {
-            temp->prev = NULL;
-            temp->next = start;
-            start->prev = temp;
-            start = temp;
+            end->next = temp;
+            end = temp;
         }
     }
-    // Function to traversal/print double linked list (START to END)
-    void traversal_S_to_E()
+    // Function to traversal/print single linked list
+    void traversal()
     {
         if (start == NULL)
         {
@@ -55,25 +50,7 @@ public:
             }
         }
     }
-    // Function to traversal/print double linked list (END to START)
-    void traversal_E_to_S()
-    {
-        if (end == NULL)
-        {
-            cout << "Underflow" << endl;
-        }
-        else
-        {
-            curr = end;
-            while (curr != NULL)
-            {
-                cout << curr->data << endl;
-                curr = curr->prev;
-            }
-        }
-    }
 };
-// MAIN FUNCTION
 int main()
 {
     system("cls");
@@ -84,12 +61,10 @@ int main()
     {
         cout << "\nEnter value to be inserted ";
         cin >> d;
-        list.insert_beginning(d);
+        list.insert_end(d);
         cout << "\n\nCont...(y / n)";
         cin >> ans;
     } while (ans == 'y' || ans == 'Y');
-    cout << "\n Value of list from start to end \n";
-    list.traversal_S_to_E();
-    cout << "\n Value of list from end to start \n";
-    list.traversal_E_to_S();
+    cout << "\n Value are \n";
+    list.traversal();
 }
