6a9e31d320a08a2128fe0388b4413fdf84601e0e
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-30 19:07:50 +0530

    Add files via upload

---
+++ Main12.cpp
@@ -0,0 +1,37 @@
+// Write a C++ program to claculate factorial of given numbers using constructor.
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class Facto
+{
+private:
+    int n;
+
+public:
+    Facto(int N)
+    {
+        n = N;
+    }
+    void printdata()
+    {
+        cout << "\nGiven number = " << n;
+    }
+    void Calsu(); // Function Declaration.
+};
+void Facto::Calsu() // Function Definition.
+{
+    int I;
+    long F = 1;
+    for (I = 1; I <= n; I++)
+    {
+        F = F * I;
+    }
+    cout << "\nFactorial Value = " << F;
+}
+int main()
+{
+    system("cls");
+    Facto x(5); // Constructor Facto() called automatically when object is created.
+    x.printdata();
+    x.Calsu();
+}
