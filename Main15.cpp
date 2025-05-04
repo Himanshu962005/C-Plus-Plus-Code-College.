9082a8e97a4a1908cee84e1f0fa0754e1ec05bac
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-05-04 17:58:15 +0530

    Add files via upload

---
+++ Main15.cpp
@@ -0,0 +1,15 @@
+// Sum of 2 Numbers.
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int A, B, S;
+    system("cls");
+    cout << "Input First Number = ";
+    cin >> A;
+    cout << "Input Second Number = ";
+    cin >> B;
+    S = A + B;
+    cout << "Sum = " << S;
+}
