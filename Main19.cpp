f93420d4895dfee18f1a33ee7b188a9cef6b6f33
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-05-04 19:15:12 +0530

    Add files via upload

---
+++ Main19.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class ADD
+{
+    int A, B;
+
+public:
+    void getdata(int a, int b)
+    {
+        A = a;
+        B = b;
+    }
+    void printdata()
+    {
+        cout << "First Number = " << A;
+        cout << "\nSecond Number = " << B;
+    }
+    void calc()
+    {
+        int S;
+        S = A + B;
+        cout << "\nSum = " << S;
+    }
+};
+int main()
+{
+    system("cls");
+    ADD x;
+    x.getdata(10, 20);
+    x.printdata();
+    x.calc();
+}
