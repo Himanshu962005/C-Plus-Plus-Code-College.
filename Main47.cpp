0bb36ee3c9dae8ea132c7ef6540d68cfddfd548b
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-05-16 19:40:52 +0530

    Add files via upload

---
+++ Main47.cpp
@@ -0,0 +1,61 @@
+// Write a program to find sum of two number and it also calculate average of both using Multiple Inheritance.
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class One
+{
+public:
+    int A;
+    void getdata(int a)
+    {
+        A = a;
+    }
+    void printdata()
+    {
+        cout << "\nFirst Number = " << A;
+    }
+};
+class Two
+{
+public:
+    int B;
+    void setdata(int b)
+    {
+        B = b;
+    }
+    void diaplaydata()
+    {
+        cout << "\nSecond Number = " << B;
+    }
+};
+class Sum : public One, public Two
+{
+public:
+    void CalcSum()
+    {
+        int S = A + B;
+        cout << "\nSum = " << S;
+    }
+};
+class Average : public Sum
+{
+public:
+    void CalcAvg()
+    {
+        float Avg = (float)(A + B) / 2;
+        cout << "\nAverage = " << Avg;
+    }
+};
+int main()
+{
+    system("cls");
+    Average x;
+    x.getdata(9);
+    x.setdata(6);
+    x.printdata();
+    x.diaplaydata();
+    x.CalcSum();
+    x.CalcAvg();
+    getch();
+    return 0;
+}
