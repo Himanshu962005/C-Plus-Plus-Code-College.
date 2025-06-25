c9d897a977a75620c6e49519c9d9a908ec785791
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-06-25 16:09:11 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,18 +5,18 @@ int main()
 {
     int i,j,k,l,m,n;
     system("CLS");
-    cout<<"Enter first number ";
-    cin>>i;
-    cout<<"Enter second number ";
-    cin>>j;
-    k=i+j;
-    l=i-j;
-    m=i*j;
-    n=i/j;
-    cout<<"Addition is "<<k<<endl;
-    cout<<"Substraction is "<<l<<endl;
-    cout<<"Multiplication is "<<m<<endl;
-    cout<<"Division is "<<n<<endl;
+    cout << "Enter first number ";
+    cin >> i;
+    cout << "Enter second number ";
+    cin >> j;
+    k = i + j;
+    l = i - j;
+    m = i * j;
+    n = i / j;
+    cout << "Addition is " << k << endl;
+    cout << "Substraction is " << l << endl;
+    cout << "Multiplication is " << m << endl;
+    cout << "Division is " << n << endl;
     getch();
     return 0;
 } 
